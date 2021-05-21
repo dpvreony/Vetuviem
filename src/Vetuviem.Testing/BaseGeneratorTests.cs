dc406af1278f7f1ac0c631129109595f5e138b88
David Vreony <dpvreony@users.noreply.github.com>
2021-05-21 16:49:51 +0100

    add base test class

---
+++ src/Vetuviem.Testing/BaseGeneratorTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Vetuviem.SourceGenerator;
+using Xunit;
+using Xunit.Abstractions;
+using Vetuviem.SourceGenerator.GeneratorProcessors;
+
+namespace Dhgms.Nucleotide.UnitTests.Generators
+{
+
+    [ExcludeFromCodeCoverage]
+    public class BaseGeneratorTests
+    {
+        public abstract class BaseGenerateAsyncMethod<TGenerator, TGeneratorProcessor> : Foundatio.Xunit.TestWithLoggingBase
+            where TGenerator : AbstractBaseGenerator<TGeneratorProcessor>
+            where TGeneratorProcessor : AbstractGeneratorProcessor, new()
+        {
+            internal const string DefaultFilePathPrefix = "Test";
+            internal const string CSharpDefaultFileExt = "cs";
+            internal const string TestProjectName = "TestProject";
+
+            protected abstract Func<TGenerator> GetFactory();
+
+            private static Compilation CreateCompilation(string source) => CSharpCompilation.Create(
+                assemblyName: "compilation",
+                syntaxTrees: new[] { CSharpSyntaxTree.ParseText(source, new CSharpParseOptions(LanguageVersion.Preview)) },
+                references: new[] { MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location) },
+                options: new CSharpCompilationOptions(OutputKind.ConsoleApplication)
+            );
+
+            private static GeneratorDriver CreateDriver(Compilation compilation, params ISourceGenerator[] generators) => CSharpGeneratorDriver.Create(
+                generators: ImmutableArray.Create(generators),
+                additionalTexts: ImmutableArray<AdditionalText>.Empty,
+                parseOptions: (CSharpParseOptions)compilation.SyntaxTrees.First().Options,
+                optionsProvider: null
+            );
+
+            private static Compilation RunGenerators(Compilation compilation, out ImmutableArray<Diagnostic> diagnostics, params ISourceGenerator[] generators)
+            {
+                CreateDriver(compilation, generators).RunGeneratorsAndUpdateCompilation(compilation, out var updatedCompilation, out diagnostics);
+                return updatedCompilation;
+            }
+
+
+            [Fact]
+            public async Task GeneratesCode()
+            {
+                var factory = GetFactory();
+
+                var instance = factory();
+                var comp = CreateCompilation(string.Empty);
+                var newComp = RunGenerators(
+                    comp,
+                    out var generatorDiags,
+                    instance);
+            }
+
+            protected BaseGenerateAsyncMethod(ITestOutputHelper output) : base(output)
+            {
+            }
+        }
+    }
+}
