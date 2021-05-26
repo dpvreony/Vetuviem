1a9d2f3a15076cc9953765d0da4ea21b649c01fa
David Vreony <dpvreony@users.noreply.github.com>
2021-05-26 16:48:07 +0100

    initial work for winui sourcegen

---
+++ src/ReactiveUI.WinUI3.VetuviemGenerator/WinUi3ViewBindingModelGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using Vetuviem.SourceGenerator;
+using Vetuviem.SourceGenerator.Features.Core;
+
+namespace ReactiveUI.WinUI3.VetuviemGenerator
+{
+    [Generator]
+    public sealed class WinUi3ViewBindingModelGenerator : AbstractViewBindingModelGenerator
+    {
+        protected override IPlatformResolver GetPlatformResolver()
+        {
+            return new WinUi3PlatformResolver();
+        }
+    }
+}
