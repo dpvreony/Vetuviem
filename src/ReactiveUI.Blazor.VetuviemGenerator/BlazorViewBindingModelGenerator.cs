90daea53e9cd648881a7bbabd056984e643d5bbe
David Vreony <dpvreony@users.noreply.github.com>
2021-05-29 16:53:13 +0100

    initial blazor logic

---
+++ src/ReactiveUI.Blazor.VetuviemGenerator/BlazorViewBindingModelGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+using Vetuviem.SourceGenerator;
+using Vetuviem.SourceGenerator.Features.Core;
+
+namespace ReactiveUI.Blazor.VetuviemGenerator
+{
+    [Generator]
+    public sealed class BlazorViewBindingModelGenerator : AbstractViewBindingModelGenerator
+    {
+        protected override MetadataReference CheckIfShouldAddMissingAssemblyReference(string assemblyOfInterest)
+        {
+            return null;
+        }
+
+        protected override IPlatformResolver GetPlatformResolver()
+        {
+            return new BlazorPlatformResolver();
+        }
+    }
+}
