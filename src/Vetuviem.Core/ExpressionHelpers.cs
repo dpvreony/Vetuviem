512a120607c6d5490d71c408898b5c184739492c
David Vreony <dpvreony@users.noreply.github.com>
2021-06-07 22:09:37 +0100

    add view to control expression helper

---
+++ src/Vetuviem.Core/ExpressionHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Vetuviem.Core
+{
+    public static class ExpressionHelpers
+    {
+        public static Expression<Func<TView, object>> GetControlPropertyExpressionFromViewExpression<TView, TProp>(
+            Expression<Func<TView, TProp>> viewPropExpression,
+            string propertyName)
+        {
+            var member = typeof(TProp).GetProperty(propertyName);
+            var memberAccess = Expression.MakeMemberAccess(
+                viewPropExpression.Body,
+                member);
+
+            var propertyExpression = Expression.Lambda<Func<TView, object>>(
+                    memberAccess,
+                    viewPropExpression.Parameters);
+
+            return propertyExpression;
+
+        }
+    }
+}
