4fb323fbc89ef256ecf139d8166071b99c477931
David Vreony <dpvreony@users.noreply.github.com>
2021-06-10 22:03:23 +0100

    add interfaces for driving rx-ui binding

---
+++ src/Vetuviem.Core/IEnableViewToViewModelBindings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ReactiveUI;
+
+namespace Vetuviem.Core
+{
+    public interface IEnableViewToViewModelBindings<TView, TViewModel>
+        where TView : class, IViewFor<TViewModel>
+        where TViewModel : class, IReactiveObject
+    {
+        IEnumerable<IViewBindingModel<TView, TViewModel>> GetBindings();
+    }
+}
---
+++ src/Vetuviem.Core/IViewBindingModel.cs
@@ -0,0 +1,10 @@
+﻿using ReactiveUI;
+
+namespace Vetuviem.Core
+{
+    public interface IViewBindingModel<TView, TViewModel>
+        where TView : class, IViewFor<TViewModel>
+        where TViewModel : class, IReactiveObject
+    {
+    }
+}
