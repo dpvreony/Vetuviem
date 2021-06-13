d7b4f7576d4d10cde7a37715d1e37605b9dcedc1
David Vreony <dpvreony@users.noreply.github.com>
2021-06-13 16:58:01 +0100

    helps to make the wpf sample vm inpc

--- src/ReactiveUI.WPF.SampleApp/ViewModels/QuestionnaireViewModel.cs
+++ src/ReactiveUI.WPF.SampleApp/ViewModels/QuestionnaireViewModel.cs
@@ -4,19 +4,56 @@ namespace ReactiveUI.WPF.SampleApp.ViewModels
 {
     public sealed class QuestionnaireViewModel : ReactiveObject
     {
-        public string Forename { get; set; }
+        private string _forename;
+        private string _surname;
+        private string _answerOne;
+        private string _answerTwo;
+        private string _answerThree;
+        private string _answerFour;
+        private string _answerFive;
 
-        public string Surname { get; set; }
 
-        public string AnswerOne { get; set; }
+        public string Forename
+        {
+            get => _forename;
+            set => this.RaiseAndSetIfChanged(ref _forename, value);
+        }
 
-        public string AnswerTwo { get; set; }
+        public string Surname
+        {
+            get => _surname;
+            set => this.RaiseAndSetIfChanged(ref _surname, value);
+        }
 
-        public string AnswerThree { get; set; }
+        public string AnswerOne
+        {
+            get => _answerOne;
+            set => this.RaiseAndSetIfChanged(ref _answerOne, value);
+        }
 
-        public string AnswerFour { get; set; }
+        public string AnswerTwo
+        {
+            get => _answerTwo;
+            set => this.RaiseAndSetIfChanged(ref _answerTwo, value);
+        }
 
-        public string AnswerFive { get; set; }
+        public string AnswerThree
+        {
+            get => _answerThree;
+            set => this.RaiseAndSetIfChanged(ref _answerThree, value);
+        }
+
+        public string AnswerFour
+        {
+            get => _answerFour;
+            set => this.RaiseAndSetIfChanged(ref _answerFour, value);
+        }
+
+        public string AnswerFive
+        {
+            get => _answerFive;
+            set => this.RaiseAndSetIfChanged(ref _answerFive, value);
+        }
 
         public int MaxLength => 50;
     }
