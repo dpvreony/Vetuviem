7a5eea7b41b57bc98a9e4feb73e5d77a1bc83939
David Vreony <dpvreony@users.noreply.github.com>
2021-06-28 22:46:16 +0100

    prep for droid and uwp samples (#57)
    * prep for droid and uwp samples
    
    * add blazor sample
    
    * remove xamarin sample for now
    
    * update reactiveui blazor
    
    * fixed use of blazor vbm lib

---
+++ src/ReactiveUI.VetuviemSample.XamDroidApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2021 DHGMS Solutions and Contributors. All rights reserved.
+// DHGMS Solutions and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReactiveUI.VetuviemSample.XamDroidApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReactiveUI.VetuviemSample.XamDroidApp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
