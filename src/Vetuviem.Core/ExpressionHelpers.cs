6807e01a530a474d35f69808ec6603676be08db3
dpvreony <dpvreony@users.noreply.github.com>
2022-04-19 09:03:36 +0100

    more xmldoc

--- src/Vetuviem.Core/ExpressionHelpers.cs
+++ src/Vetuviem.Core/ExpressionHelpers.cs
@@ -7,8 +7,20 @@ using System.Linq.Expressions;
 
 namespace Vetuviem.Core
 {
+    /// <summary>
+    /// Helper for manipulating the view expressions.
+    /// </summary>
     public static class ExpressionHelpers
     {
+        /// <summary>
+        /// Takes a view property expression and rewrites it to target the control property.
+        /// </summary>
+        /// <typeparam name="TView">The type for the view.</typeparam>
+        /// <typeparam name="TProp">The type for the property, which will be a control in the different UI frameworks.</typeparam>
+        /// <typeparam name="TResult">The type for the bound result.</typeparam>
+        /// <param name="viewPropExpression">Expression representing the view property.</param>
+        /// <param name="propertyName">The name of the property to target.</param>
+        /// <returns>Expression for binding to a control property.</returns>
         public static Expression<Func<TView, TResult>> GetControlPropertyExpressionFromViewExpression<TView, TProp, TResult>(
             Expression<Func<TView, TProp>> viewPropExpression,
             string propertyName)
@@ -26,13 +38,13 @@ namespace Vetuviem.Core
         }
 
         /// <summary>
-        /// Converts an expression for a property on a control to be an expression to a base class
+        /// Converts an expression for a property on a control to be an expression to a base class.
         /// </summary>
         /// <typeparam name="TView">The type for view the control is on.</typeparam>
         /// <typeparam name="TProp">The type for the property.</typeparam>
         /// <typeparam name="TResult">The base type for the property.</typeparam>
         /// <param name="viewPropExpression">Expression representing the derived control.</param>
-        /// <returns></returns>
+        /// <returns>Expression for targeting the member on the base class.</returns>
         public static Expression<Func<TView, TResult>> ConvertControlExpressionToBaseClassExpression<TView, TProp, TResult>(Expression<Func<TView, TProp>> viewPropExpression)
             where TProp : class, TResult
             where TResult : class
--- src/Vetuviem.Core/IEnableViewToViewModelBindings.cs
+++ src/Vetuviem.Core/IEnableViewToViewModelBindings.cs
@@ -10,14 +10,14 @@ namespace Vetuviem.Core
     /// <summary>
     /// Represents a View to View Model Binding.
     /// </summary>
-    /// <typeparam name="TView"></typeparam>
-    /// <typeparam name="TViewModel"></typeparam>
+    /// <typeparam name="TView">The type for the view.</typeparam>
+    /// <typeparam name="TViewModel">The type for the viewmodel.</typeparam>
     public interface IEnableViewToViewModelBindings<in TView, in TViewModel>
         where TView : class, IViewFor<TViewModel>
         where TViewModel : class, IReactiveObject
     {
         /// <summary>
-        /// Apply control bindings between a View and ViewModel
+        /// Apply control bindings between a View and ViewModel.
         /// </summary>
         /// <param name="disposeWithAction">The ReactiveUI Disposal Tracker.</param>
         /// <param name="view">Instance of the view.</param>
