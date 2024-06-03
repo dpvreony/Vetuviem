43011dda77559885a60e32cd165aa6b3ee3a7d5d
David Vreony <dpvreony@users.noreply.github.com>
2024-06-03 19:10:23 +0100

    feature: logic for handling assembly replacement via config (#404)
    * logic for handling assembly replacement via config
    
    * fix issue with assembly arg not being ignored if empty

---
+++ src/Vetuviem.SourceGenerator/Features/Configuration/AssemblyMode.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2022 DPVreony and Contributors. All rights reserved.
+// DPVreony and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Vetuviem.SourceGenerator.Features.Configuration
+{
+    /// <summary>
+    /// The operation to carry out if assemblies are specified in the config.
+    /// </summary>
+    public enum AssemblyMode
+    {
+        /// <summary>
+        /// None, this is a language default to pickup invalid values.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Replace the default platform assemblies with the specified assemblies.
+        /// </summary>
+        Replace,
+
+        /// <summary>
+        /// Extend the default platform assemblies with the specified assemblies.
+        /// </summary>
+        Extend
+    }
+}
