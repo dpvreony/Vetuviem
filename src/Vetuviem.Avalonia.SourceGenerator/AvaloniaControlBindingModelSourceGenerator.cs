7c3e2391eaa00f87b15394fb10a0372865d5c53d
David Vreony <dpvreony@users.noreply.github.com>
2024-06-13 18:28:19 +0100

    add avalonia generation

---
+++ src/Vetuviem.Avalonia.SourceGenerator/AvaloniaControlBindingModelSourceGenerator.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2022 DPVreony and Contributors. All rights reserved.
+// DPVreony and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis;
+using Vetuviem.SourceGenerator;
+using Vetuviem.SourceGenerator.Features.Core;
+
+namespace Vetuviem.Avalonia.SourceGenerator
+{
+    /// <summary>
+    /// Control Binding Model Source Generator for Blazor.
+    /// </summary>
+    [Generator]
+    public sealed class AvaloniaControlBindingModelSourceGenerator : AbstractControlBindingModelSourceGenerator
+    {
+        /// <inheritdoc />
+        protected override MetadataReference? CheckIfShouldAddMissingAssemblyReference(string assemblyOfInterest)
+        {
+            return null;
+        }
+
+        /// <inheritdoc />
+        protected override IPlatformResolver GetPlatformResolver()
+        {
+            return new AvaloniaPlatformResolver();
+        }
+
+        /// <inheritdoc />
+        protected override string GetPlatformName()
+        {
+            return "Avalonia";
+        }
+    }
+}
