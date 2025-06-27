a83c685529b94d442fff97e8369fbc664418454f
David Vreony <dpvreony@users.noreply.github.com>
2025-06-27 20:56:26 +0100

    Feature: add project level source generation (#514)
    * Create WpfProjectControlBindingModelSourceGenerator.cs
    
    * project gen now picking up controls
    
    * initial population of project based gen
    
    * add a sub namespace
    
    * produce abstraction for project level generator
    
    * Create WinUi3ProjectControlBindingModelSourceGenerator.cs
    
    * add avalonia, blazor, maui, winforms project level gen
    
    * Update version.json
    
    * remove transient pkf pickup on test projects

---
+++ src/Vetuviem.Maui.SourceGenerator/MauiProjectControlBindingModelSourceGenerator.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2022 DPVreony and Contributors. All rights reserved.
+// DPVreony and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis;
+using Vetuviem.SourceGenerator;
+using Vetuviem.SourceGenerator.Features.Core;
+
+namespace Vetuviem.Maui.SourceGenerator
+{
+    /// <summary>
+    /// Project level Control Binding Model Source Generator for WPF.
+    /// </summary>
+    [Generator]
+    public sealed class MauiProjectControlBindingModelSourceGenerator : AbstractProjectControlBindingModelSourceGenerator
+    {
+        /// <inheritdoc />
+        protected override string GetPlatformName() => "Maui";
+
+        /// <inheritdoc/>
+        protected override IPlatformResolver GetPlatformResolver() => new MauiPlatformResolver();
+    }
+}
---
+++ src/Vetuviem.WinUi.SourceGenerator/WinUi3ProjectControlBindingModelSourceGenerator.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2022 DPVreony and Contributors. All rights reserved.
+// DPVreony and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis;
+using Vetuviem.SourceGenerator;
+using Vetuviem.SourceGenerator.Features.Core;
+
+namespace Vetuviem.WinUi.SourceGenerator
+{
+    /// <summary>
+    /// Project level Control Binding Model Source Generator for WPF.
+    /// </summary>
+    [Generator]
+    public sealed class WinUI3ProjectControlBindingModelSourceGenerator : AbstractProjectControlBindingModelSourceGenerator
+    {
+        /// <inheritdoc />
+        protected override string GetPlatformName() => "WinUi3";
+
+        /// <inheritdoc/>
+        protected override IPlatformResolver GetPlatformResolver() => new WinUi3PlatformResolver();
+    }
+}
