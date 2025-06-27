879e0ca95fae2c8d1cde52adfb2e099f4740f317
David Vreony <dpvreony@users.noreply.github.com>
2025-06-27 20:13:21 +0100

    Create WinUi3ProjectControlBindingModelSourceGenerator.cs

---
+++ src/Vetuviem.WinUi.SourceGenerator/WinUi3ProjectControlBindingModelSourceGenerator.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2022 DPVreony and Contributors. All rights reserved.
+// DPVreony and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis;
+using Vetuviem.SourceGenerator;
+using Vetuviem.SourceGenerator.Features.Core;
+using Vetuviem.WinUi.SourceGenerator;
+
+namespace Vetuviem.WPF.SourceGenerator
+{
+    /// <summary>
+    /// Project level Control Binding Model Source Generator for WPF.
+    /// </summary>
+    [Generator]
+    public sealed class WinUI3ProjectControlBindingModelSourceGenerator : AbstractProjectControlBindingModelSourceGenerator
+    {
+        /// <inheritdoc />
+        protected override string GetPlatformName() => "WinUi3";
+
+        /// <inheritdoc/>
+        protected override IPlatformResolver GetPlatformResolver() => new WinUi3PlatformResolver();
+    }
+}
