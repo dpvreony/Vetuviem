57f53048b4d5b7b2ea83f5e0df8ceabd59c5ec92
David Vreony <dpvreony@users.noreply.github.com>
2025-06-27 10:35:15 +0100

    Create WpfProjectControlBindingModelSourceGenerator.cs

---
+++ src/Vetuviem.WPF.SourceGenerator/WpfProjectControlBindingModelSourceGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Vetuviem.WPF.SourceGenerator
+{
+    [Generator]
+    public sealed class WpfProjectControlBindingModelSourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            var classDeclarations = context.SyntaxProvider
+                .CreateSyntaxProvider(
+                    predicate: (s, _) => s is ClassDeclarationSyntax,
+                    transform: (ctx, _) => (ClassDeclarationSyntax)ctx.Node)
+                .Where(cls => cls != null);
+
+            context.RegisterSourceOutput(classDeclarations, (spc, cls) =>
+            {
+                spc.AddSource(cls.Identifier.ToFullString(), SourceText.From("// Classes collected", Encoding.UTF8));
+            });
+        }
+    }
+}
