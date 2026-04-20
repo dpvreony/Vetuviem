faf5b5da51cc376397e9bc547ae0e058ef468b5a
David Vreony <dpvreony@users.noreply.github.com>
2026-04-20 21:10:28 +0100

    add skeleton of factory method

--- src/Vetuviem.SourceGenerator/Features/ControlBindingModels/BindingHelperExtensionClassGenerator.cs
+++ src/Vetuviem.SourceGenerator/Features/ControlBindingModels/BindingHelperExtensionClassGenerator.cs
@@ -2,6 +2,8 @@
 // DPVreony and Contributors licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using System;
+using System.Reflection.Metadata;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -35,10 +37,50 @@ namespace Vetuviem.SourceGenerator.Features.ControlBindingModels
                 SyntaxFactory.Token(SyntaxKind.StaticKeyword),
                 SyntaxFactory.Token(SyntaxKind.PartialKeyword));
 
+            var returnType = GetReturnType(namedTypeSymbol);
+
+            // TODO: add the expression argument
+            // TODO: add other arguments as needed to determine which properties to set on the binding model instance.
+            // TODO: add the method body that creates and returns an instance of the binding model, using the expression argument to determine which properties to set on the binding model instance.
+            var parameters = RoslynGenerationHelpers.GetParams(new[]
+            {
+                $"this Expression<Func<TView, {namedTypeSymbol.GetFullName()}>> controlExpression",
+            });
+
+            var methodBody = SyntaxFactory.Block(
+                SyntaxFactory.ParseStatement($"var bindingModel = new {returnType}();"),
+                SyntaxFactory.ParseStatement("// TODO: use the controlExpression to determine which properties to set on the binding model instance."),
+                SyntaxFactory.ParseStatement("return bindingModel;"));
+
+            var defaultFactoryMethodDeclaration = SyntaxFactory.MethodDeclaration(
+                returnType,
+                "GetDefaultBindingModel")
+                .AddModifiers(
+                    SyntaxFactory.Token(SyntaxKind.PublicKeyword),
+                    SyntaxFactory.Token(SyntaxKind.StaticKeyword))
+                .WithParameterList(parameters)
+                .AddBodyStatements(methodBody)
+                .WithLeadingTrivia(XmlSyntaxFactory.InheritdocSyntax);
+
+
+            var members = new SyntaxList<MemberDeclarationSyntax>(defaultFactoryMethodDeclaration);
+
             var classDeclaration = SyntaxFactory.ClassDeclaration(classNameIdentifier)
-                .WithModifiers(modifiers);
+                .WithModifiers(modifiers)
+                .WithMembers(members);
 
             return classDeclaration;
+        }
+
+        /// <inheritdoc />
+        private static TypeSyntax GetReturnType(INamedTypeSymbol namedTypeSymbol)
+        {
+            if (namedTypeSymbol == null)
+            {
+                throw new ArgumentNullException(nameof(namedTypeSymbol));
+            }
+
+            return SyntaxFactory.ParseTypeName($"{namedTypeSymbol.Name}ControlBindingModel");
         }
     }
 }
